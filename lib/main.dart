e14e03100cab0207a48a3bde9bb0b9b59b79155c
Ruan Emanuell <ruanvitorcom540@gmail.com>
2022-09-23 22:59:43 -0300

    Final version

--- lib/main.dart
+++ lib/main.dart
@@ -21,52 +21,80 @@ void main(){
 
 
 class MyApp extends StatelessWidget{
-  
-
   @override
   
   Widget build(BuildContext context){
+    var screenHeight=MediaQuery.of(context).size.height;
+    var screenWidth=MediaQuery.of(context).size.width;
     return Scaffold(
-        body:Center(
-          child: Column(
-            mainAxisAlignment:MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Gametile(
-                screen:VegetaPlay(),
-                image:"assets/images/vegetamenu.png",
-                title:"Vegeta's Workout",
-                color:Colors.blue,
-                text:"That damn Kakarot. I promise you I can surpass that bastard once I do 1000 push-ups...",
-                buttonColor:Color.fromARGB(255, 15, 112, 192)
-              ),
-              Gametile(
-                screen:GokuPlay(),
-                image:"assets/images/gokumenu.png",
-                title:"Goku's Lunch Time",
-                color:Colors.orange,
-                text:"Maan I'm hungry! I need to eat so I can go to King Kai's planet to train. I wonder what would happen if I eat all the food...",
-                buttonColor:Color.fromARGB(255, 255, 134, 64)
-              ),
-              Gametile(
-                screen:SnakeWayPlay(),
-                image:"assets/images/snakewaymenu.png",
-                title:"Snakeway Run",
-                color:Color.fromARGB(255, 231, 214, 55),
-                text:"The Saiyans are coming to Earth! I gotta hurry and run as fast as I can to arrive at King Kai's planet!",
-                buttonColor:Color.fromARGB(255, 211, 197, 74),
-              ),
-              Gametile(
-                screen:GotenksPlay(),
-                image:"assets/images/gotenksmenu.png",
-                title:"Superghost Invasion",
-                color:Colors.lightBlue,
-                text:"Majin Buu is SUPER strong! I need to make an army of ghosts so I can defeat him once and for all! Maybe 50?",
-                buttonColor:Colors.blueAccent
-              )
-            ],
+        body:Container(
+          decoration:BoxDecoration(
+            color:Color.fromARGB(71, 114, 161, 199),
+            image:DecorationImage(
+              image:AssetImage("assets/images/dragonballs.png"), 
+              opacity:0.5
+            )
           ),
-        )
+          child: Stack(
+            children:[
+              Center(
+              child:Column(
+                  mainAxisAlignment:MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: const StrokeText(
+                        text:"Choose the mode you wanna play", 
+                        strokeWidth:2,
+                        textStyle:TextStyle(
+                        fontSize:20,
+                        color:Colors.white,
+                        fontFamily:"Tahoma",
+                        fontWeight:FontWeight.bold
+                      ))
+                    ),
+                    Gametile(
+                      screen:VegetaPlay(),
+                      image:"assets/images/vegetamenu.png",
+                      title:"Vegeta's Workout",
+                      color:Colors.blue,
+                      text:"That damn Kakarot. I promise you I can surpass that bastard once I do 1000 push-ups...",
+                      buttonColor:Color.fromARGB(255, 15, 112, 192)
+                    ),
+                    Gametile(
+                      screen:GokuPlay(),
+                      image:"assets/images/gokumenu.png",
+                      title:"Goku's Lunch Time",
+                      color:Colors.orange,
+                      text:"Maan I'm hungry! I need to eat so I can go to King Kai's planet to train. I wonder what would happen if I eat all the food...",
+                      buttonColor:Color.fromARGB(255, 255, 134, 64)
+                    ),
+                    Gametile(
+                      screen:SnakeWayPlay(),
+                      image:"assets/images/snakewaymenu.png",
+                      title:"Snakeway Run",
+                      color:Color.fromARGB(255, 231, 214, 55),
+                      text:"The Saiyans are coming to Earth! I gotta hurry and run as fast as I can to arrive at King Kai's planet!",
+                      buttonColor:Color.fromARGB(255, 192, 178, 54),
+                    ),
+                    Gametile(
+                      screen:GotenksPlay(),
+                      image:"assets/images/gotenksmenu.png",
+                      title:"Superghost Invasion",
+                      color:Colors.lightBlue,
+                      text:"Majin Buu is SUPER strong! I need to make an army of ghosts so I can defeat him once and for all! Maybe 50?",
+                      buttonColor:Color.fromARGB(255, 31, 100, 219)
+                    )
+                  ],
+                )
+              ),
+              Container(
+                margin:EdgeInsets.only(top: screenHeight/1.25, left:screenWidth/1.4),
+                child:Image.asset("assets/images/gokutitle.png")
+              )
+             ]
+          ),
+        ),
       );
   }
 }
